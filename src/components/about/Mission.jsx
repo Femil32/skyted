e217ea1e97dfa60521acb6a71f404ad2487ac111
Femil Savaliya <femilsavaliya555@gmail.com>
2023-01-24 01:07:22 +0530

    update apis for home,about,ces

--- src/components/about/Mission.jsx
+++ src/components/about/Mission.jsx
@@ -1,38 +1,31 @@
 import React from "react";
-import OneraIMG from "../../assets/imgs/about/onera.png";
-import VRGirlIMG from "../../assets/imgs/about/vr-girl.png";
+import { getImg, parseHtml } from "../../helpers";
 import ImgData from "../common/ImgData";
 import { CustomIMG } from "../Micro";
 
-function Mission() {
+function Mission({ response, boxResponse }) {
   return (
     <section className="section-container xl:py-[88px] lg:py-16 md:py-12 py-10">
       <ImgData
-
-        title="Mission of Skyted"
-        description={`Skyted scouts many new technologies to incorporate the best technology
-        bricks to offer millions of users a safe and relaxed conversation where
-        they are located. We will provide a full array of technology for the Future of
-        Work as anyone has the right to create and control his own sound bubble.`}
-        src={VRGirlIMG}
+        title={response?.Title}
+        description={parseHtml(response?.Description)}
+        src={getImg(response?.Image)}
         alt="VRGirlIMG"
       />
       <div className="grid grid-cols-12 xl:py-24 lg:pt-16 md:pt-14 pt-12 pb-5">
         <div
-
           className="col-start-1 md:col-start-2 col-span-4 row-span-full bg-white z-10 flex justify-center items-center my-3 lg:my-4 rounded-2xl ring"
         >
-          <CustomIMG src={OneraIMG} alt="onera" className="w-80" />
+          <CustomIMG src={getImg(boxResponse?.CompanyImage)} alt="onera" className="w-80" />
         </div>
         <div
-
           className="col-span-9 md:col-span-8 md:col-start-5 md:col-end-12  ml-12 col-start-4 row-span-full bg-gradient-to-r from-cyan-100 to-blue-100 rounded-xl p-6 pl-12 sm:p-8 sm:pl-20 md:pl-28 lg:p-16 lg:pl-36"
         >
           <h2 className="font-bold xl:text-3xl lg:text-2xl md:text-xl  text-lg sm:mb-4 mb-2">
-            ONERA Tech Support
+            {boxResponse?.CompanyTitle}
           </h2>
           <span className="text-gray-500 lg:text-base text-sm">
-            The Frech aerospace lab is our key partner in this project.
+            {boxResponse?.CompanyDescription}
           </span>
         </div>
       </div>
--- src/components/ces/Hero.jsx
+++ src/components/ces/Hero.jsx
@@ -1,50 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { getImg, parseHtml } from "../../helpers";
 import { CtaBtn, CustomIMG, SlatePara } from "../Micro";
-import Cse2023 from "../../assets/imgs/Group 2506.png";
 
-function Hero() {
+function Hero({ response }) {
   return (
     <section className="section-container">
       <div className="flex gap-2 py-12 w-full max-md:flex-col max-md:gap-8">
         <div className="flex flex-col gap-6 md:w-3/5">
           <h6 className="red-header">
-            Consumer Technology Association
+            {response?.HeroTitle}
           </h6>
           <h1
-
-
             className="text-5xl font-bold max-w-2xl leading-[55px]"
           >
-            Come and try the mask at&nbsp;
-            <span className="text-twitter">Skyted Booth</span>
+            {response?.HeroSubtitle}
+            {/* Come and try the mask at&nbsp;
+            <span className="text-twitter">Skyted Booth</span> */}
           </h1>
           <div className="space-y-6 max-w-2xl">
             <SlatePara
-
-
-              text={`Perfect for the hybrid work, this wearable offers “new nomads” total comfort
-              and speech privacy in aircraft, transportation, open spaces and in the virtual
-              world as metavers and online games.`}
-            />
-            <SlatePara
-              className="text-black"
-
-
-              text="Come and see us on booth 61211-10 (5th Jan’23 - 31th Jan’23) at Eureka Park to get a glimpse of our project. Please delete other line."
-            />
-            <SlatePara
-
-
-              text={`Skyted has been selected by Airbus Dev, Aerospace Valley, Starburst VC and
-              collaborates with CNRS and ESA for this project.`}
+              text={parseHtml(response?.HeroDescription)}
             />
           </div>
           <div>
             <a href="https://drive.google.com/file/d/1ve3d4awVstp8AgUhJSwM0p7-MzT30dXJ/view?usp=share_link" target="_blank" rel="noreferrer">
               <CtaBtn
-
-
                 text="CES Media Kit"
                 className="text-white bg-twitter inline-block"
               />
@@ -52,11 +32,9 @@ function Hero() {
           </div>
         </div>
         <div
-
-
           className="flex justify-center items-center"
         >
-          <CustomIMG src={Cse2023} alt="Cse2023" className="max-w-md" />
+          <CustomIMG src={getImg(response?.HeroImage)} alt="Cse2023" className="max-w-md" />
         </div>
 
       </div>
