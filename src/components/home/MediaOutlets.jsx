a05db21913fb3ef95256397bcc606b25b1f2449f
Femil Savaliya <femilsavaliya555@gmail.com>
2023-01-26 20:02:55 +0530

    responsive update

--- src/components/home/MediaOutlets.jsx
+++ src/components/home/MediaOutlets.jsx
@@ -8,10 +8,10 @@ function MediaOutlets({ response }) {
   return (
     <section className="section-container lg:py-[60px] bg-white text-black border-b">
       <PageHeader title="Media Outlets" className="text-center max-md:mb-12" />
-      <div className="container mx-auto flex justify-center items-center gap-12">
+      <div className="container mx-auto flex justify-center items-stretch flex-row gap-12 max-md:flex-wrap">
         {response?.MediaOutletItem?.map((patner) => (
-          <div className="w-full h-48 overflow-hidden" key={patner.id}>
-            <CustomIMG src={getImg(patner?.MediaOutlestImage)} ImgClassName="w-full object-contain aspect-[2/3] h-full" className="w-full h-full flex justify-center items-center" />
+          <div className="md:w-full w-32 h-auto overflow-hidden" key={patner.id}>
+            <CustomIMG src={getImg(patner?.MediaOutlestImage)} ImgClassName="w-full object-contain" className="w-full h-full flex justify-center items-center" />
           </div>
         ))}
       </div>
